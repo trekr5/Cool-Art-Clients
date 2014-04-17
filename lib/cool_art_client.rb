c74116aeee84de829d5e92a57949b3745079aaed
angela ebirim <angelaebirim@Jacks-MacBook-Air-2.local>
2014-04-17 11:59:23 +0100

    creating and posting ticket

--- lib/cool_art_client.rb
+++ lib/cool_art_client.rb
@@ -3,25 +3,34 @@ require 'pickled_shark'
 
 class CoolArtClient < Sinatra::Base
 
-get "/" do 
-  erb :index
-end
+  get "/" do 
+    erb :index
+  end
 
-get "/galleries" do
-   @galleries = PickledShark::Gallery.all["galleries"]
-   erb :galleries
-end
+  get "/galleries" do
+     @galleries = PickledShark::Gallery.all["galleries"]
+     erb :galleries
+  end
 
-get "/galleries/:id" do
-   @gallery = PickledShark::Gallery.get_gallery(params[:id])
-   erb :gallery
-end
+  get "/galleries/:id" do
+     @gallery = PickledShark::Gallery.get_gallery(params[:id])
+     erb :gallery
+  end
 
-get "/galleries/:id/exhibitions" do
-  @exhibition = PickledShark::Exhibition.get_exhibitions_for_gallery(params[:id])
-  erb :exhibition
-end
+  get "/galleries/:id/exhibitions" do
+    @exhibition = PickledShark::Exhibition.get_exhibitions_for_gallery(params[:id])
+    erb :exhibition
+  end
 
+  get "/exhibitions/:exhibition_id/tickets" do
 
+  #@ticket = PickledShark::Ticket.create(params[:name], params[:exhibition_id], params[:entry_at]) 
+    erb :create_ticket
+  end
 
+  post "/exhibitions/:exhibition_id/tickets" do
+
+    @ticket = PickledShark::Ticket.create(params[:name], params[:exhibition_id], params[:entry_at]) 
+    erb :bought_ticket
+  end
 end
